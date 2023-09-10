d33eb964317fb36d65efa2730d6155c8f39a3a45
Rafael <rafaelmagalhaes1@live.com>
2023-09-10 17:19:30 -0300

    unnecessary comments removed

--- back-to-top-button.php
+++ back-to-top-button.php
@@ -4,8 +4,6 @@
  * Description: Adicione um botão "Voltar ao Topo" ao seu site.
  * Version: 2.0
  * Author: Rafael M.
- * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: back-to-top-button
  */
 
@@ -18,7 +16,7 @@ add_action('wp_enqueue_scripts', 'add_back_to_top_assets');
 // Adiciona o botão ao final do conteúdo
 function add_back_to_top_button() {
     $plugin_dir = plugin_dir_url(__FILE__);
-    $image_url = $plugin_dir . 'images/top_arrow.png'; // Substitua 'seu-icone.png' pelo nome da sua imagem
+    $image_url = $plugin_dir . 'images/top_arrow.png';
     echo '<a href="#" id="back-to-top-button" class="back-to-top"><img class="icon" src="' . esc_url($image_url) . '" alt="Voltar ao Topo" /></a>';
 }
 add_action('wp_footer', 'add_back_to_top_button');
